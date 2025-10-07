93007ae0c6dcdcfe9aefacdb7e7366c8abcfa33f
kenqia <2696015553@qq.com>
2025-10-07 14:24:03 +0800

    Congratulations on getting the wrapping and unwrapping logic right

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -5,14 +5,24 @@ using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  debug( "unimplemented wrap( {}, {} ) called", n, zero_point.raw_value_ );
-  return Wrap32 { 0 };
+  return Wrap32(n + zero_point.raw_value_);
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  debug( "unimplemented unwrap( {}, {} ) called", zero_point.raw_value_, checkpoint );
-  return {};
+  uint32_t dis;
+  // 第一个点，可能减溢出
+  if(raw_value_ < zero_point.raw_value_) dis = (1UL << 32) - (zero_point.raw_value_ - raw_value_);
+  else dis = raw_value_ - zero_point.raw_value_;
+
+  uint64_t base = (checkpoint & 0xFFFFFFFF00000000) + dis;
+
+  // 第二个点是可能在起始
+  uint64_t ret = base;
+  if (checkpoint > base && checkpoint - base > (1UL << 31) ) {
+    ret = base + (1UL << 32);
+  } else if (base > checkpoint && base - checkpoint > (1UL << 31) && base >= (1UL << 32) ) {
+    ret = base - (1UL << 32);
+  }
+  return ret;
 }
